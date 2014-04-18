037eb584d41b45e09c425f8a653e8c37452d8efc
Ernesto Buenrostro <neto.buenrostro@gmail.com>
2014-04-18 16:46:09 -0700

    Checking for python
    Check if python 2.x exist if not we need to install it, if is in the 3.x version,
    we need 2.x

--- nexus.sh
+++ nexus.sh
@@ -9,3 +9,51 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+echo "Checking python2.7+ ..."
+# check for git
+echo "Checking git 1.7.9+ ..."
+GIT_VERSION=`which python`
+if [ $? -ne 0 ]; then
+    echo "Instaling python"
+    apt-get --yes install python2.7
+    if [ $? -ne 0 ]; then
+        echo -e "\nPlease install python 2.7 or greater but lower than python 3 to continue\n"
+        echo -e "\n sudo apt-get --yes install python2.7\n"
+        exit 1
+    fi
+fi
+PYTHON_VER=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
+
+
+PYTHON_MIN_VER="2.7"
+PYTHON_MAX_VER="3.0" # not 3 should be lower than 3
+
+
+if [[ "$PYTHON_MAX_VER" < "$PYTHON_VER" ]]; then
+    echo "Python must be 2.x, 3 is not supported yet in ansible"
+    echo "Instaling python2.7"
+    apt-get --yes install python2.7
+    if [ $? -ne 0 ]; then
+        echo -e "\nPlease install python 2.7 or greater but lower than python 3 to continue\n"
+        echo -e "\n sudo apt-get --yes install python2.7\n"
+        exit 1
+    fi
+else
+    echo "Python $PYTHON_VER found."
+fi
+
+PYTHON_VER=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
+
+
+if [[ "$PYTHON_VER" < "$PYTHON_MIN_VER" ]]; then
+    echo "Instaling python2.7"
+    apt-get --yes install python2.7
+    if [ $? -ne 0 ]; then
+        echo -e "\nPlease install python 2.7 or greater but lower than python 3 to continue\n"
+        echo -e "\n sudo apt-get --yes install python2.7\n"
+        exit 1
+    fi
+else
+    echo "Python $PYTHON_VER found."
+fi
+
